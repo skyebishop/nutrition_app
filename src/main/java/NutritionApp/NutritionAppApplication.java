a9f886587dddcc41d925c480c2bdb5219e8f646a
skyebishop <schuylerbishop3@gmail.com>
2020-12-29 15:33:36 -0500

    basics

---
+++ src/main/java/NutritionApp/NutritionAppApplication.java
@@ -0,0 +1,13 @@
+package NutritionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NutritionApp/NutritionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package NutritionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
