1224558d8765ca51bb355cad3a8e3716f350fab7
jayme <jaymeipema@gmail.com>
2020-12-30 15:09:17 -0500

    API call added and tested

---
+++ src/main/java/NutritionApp/services/NutritionixApiService.java
@@ -0,0 +1,42 @@
+package NutritionApp.services;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NutritionixApiService {
+	
+	private RestTemplate rt = new RestTemplate();
+	private HttpHeaders headers = new HttpHeaders();
+	
+	public void nutritionixTestCall() {
+		
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileInputStream("src/main/resources/config/apikeys"));
+		} catch(IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		
+		  headers.add("x-user-jwt", "0"); 
+		  headers.add("x-app-id", prop.getProperty("appid"));
+		  headers.add("x-app-key", prop.getProperty("appkey"));
+		  
+		  HttpEntity<String> entity = new HttpEntity<String>(headers);
+		  
+		  String url ="https://trackapi.nutritionix.com/v2/search/instant?query=grilled";
+		  ResponseEntity<String> json = rt.exchange(url, HttpMethod.GET, entity, String.class); 
+		  System.out.println("JSON RESPONSE:");
+		  System.out.println(json.getBody());
+		 
+	}
+}
